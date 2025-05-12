4565f32f78e877ddfb79bb92e2be14c8cd7ef32e
Pete Love <petelovedotcom@gmail.com>
2025-05-12 08:24:02 +0100

    updated how avatar props passed through

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -9,26 +9,23 @@ import './UserItem.css';
  * @interface UserItemProps
  * @property {string} name - The display name of the user
  * @property {string} [email] - Optional email address of the user
- * @property {string} [avatarImageUrl] - Optional URL for the user's avatar image
- * @property {string} avatarFallbackUrl - Fallback URL for the avatar image if primary image fails to load is it not supplied
- * @property {string} avatarAltText - Alternative text description for the avatar image
- * @property {"small" | "standard" | "large"} [size] - Optional size variant for the user item display
+ * @property {object} avatarProps - Props for the avatar component
+ * @property {string} [avatarProps.imageUrl] - Optional URL for the user's avatar image
+ * @property {string} avatarProps.fallbackUrl - Fallback URL for the avatar image
+ * @property {string} avatarProps.altText - Alternative text description for the avatar image
+ * @property {"small" | "medium" | "large"} [size] - Optional size variant for the user item display
  */
 interface UserItemProps {
   name: string;
   email?: string;
-  avatarImageUrl?: string;
-  avatarFallbackUrl: string;
-  avatarAltText: string;
+  avatarProps: {imageUrl?: string; fallbackUrl: string; altText: string};
   size?: "small" | "medium" | "large";
 }
 
 const UserItem = ({
   name,
   email,
-  avatarImageUrl,
-  avatarFallbackUrl,
-  avatarAltText,
+  avatarProps,
   size = "medium"
 }: UserItemProps) => {
 
@@ -40,7 +37,7 @@ const UserItem = ({
 
   return (
     <figure className={`user-item user-item--${size}`}>
-      <Avatar size={avatarSize[size]} imageUrl={avatarImageUrl} fallbackUrl={avatarFallbackUrl} altText={avatarAltText} />
+      <Avatar size={avatarSize[size]} imageUrl={avatarProps.imageUrl || null} fallbackUrl={avatarProps.fallbackUrl} altText={avatarProps.altText} />
       <figcaption>
         <p>{name}</p>
         {email && <p><a href={`mailto:${email}`}>{email}</a></p>}
